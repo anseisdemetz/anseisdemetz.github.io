b372405ab07807adbbe9e94609b5db2e5db6b7c6
Jérôme Courivaud <jeromecourivaud@gmail.com>
2026-07-31 15:55:14 +0200

    Homologation 1.0 test access api

--- maquette/centreh/config.js
+++ maquette/centreh/config.js
@@ -2,7 +2,7 @@
 const CONFIG = {
     // API pour récupérer la liste des codes d'homologation
     // CODES_API_URL: "https://preprod-cdiscount-api.comparecycle.com/Diagnostic/codes",
-    CODES_API_URL: "https://corsproxy.io/?" + encodeURIComponent("https://preprod-cdiscount-api.comparecycle.com/Diagnostic/codes"),
+    CODES_API_URL: "https://preprod-cdiscount-api.comparecycle.com/Diagnostic/codes",
     CODES_API_TOKEN: "xPOU6kuI1Gv4NZS1a6l05bM66p5yQ5dr",
 
     // API pour envoyer le résultat du pré-diagnostic
