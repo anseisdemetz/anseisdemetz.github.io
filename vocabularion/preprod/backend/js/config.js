aa2368ae1a7584d46cb8615a1e6875a0c26c25da
Jérôme Courivaud <jeromecourivaud@gmail.com>
2026-08-03 12:08:14 +0200

    Vocabularion Backend 1.0 /Config.js Preprod

--- vocabularion/preprod/backend/js/config.js
+++ vocabularion/preprod/backend/js/config.js
@@ -4,7 +4,7 @@ const SUPABASE_URL = 'https://okqpskyzteuhwochesbv.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9rcXBza3l6dGV1aHdvY2hlc2J2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUzOTI2MDEsImV4cCI6MjEwMDk2ODYwMX0.AHxjCrvXKpccziqOo17-FgOm7uJo7WiVuUxrrsG_eWE';
 
 // Nom de la table ciblée en préproduction
-const VOCAB_TABLE = 'vocabulary_preprod';
+const VOCAB_TABLE = 'vocabulary';
 
 // Initialisation du client Supabase
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
