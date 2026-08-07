b83b3850db0e239ca47572ecc0538074dd727e98
Jérôme Courivaud <jeromecourivaud@gmail.com>
2026-08-07 15:18:36 +0200

    ChunkMail Preprod 1.6

--- chunkmail/config/supabase.js
+++ chunkmail/config/supabase.js
@@ -1,5 +1,5 @@
 // Remplace ces valeurs par tes variables d'environnement ou constantes de projet Supabase
-const SUPABASE_URL = "https://TON-PROJET.supabase.co";
-const SUPABASE_ANON_KEY = "TA_CLE_ANON_PUBLIQUE";
+const SUPABASE_URL = "https://okqpskyzteuhwochesbv.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9rcXBza3l6dGV1aHdvY2hlc2J2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUzOTI2MDEsImV4cCI6MjEwMDk2ODYwMX0.AHxjCrvXKpccziqOo17-FgOm7uJo7WiVuUxrrsG_eWE";
 
 export const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
